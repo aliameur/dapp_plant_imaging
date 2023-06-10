ffaf51753a631a58ffafdd385479b01902669902
aliameur <aliameur169@gmail.com>
2023-06-10 21:01:26 +0100

    make auth modular

--- flask_app/api/auth/__init__.py
+++ flask_app/api/auth/__init__.py
@@ -1,20 +1,19 @@
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 from firebase_admin import auth
 
 
 def create_module(app, **kwargs):
     @app.before_request
     def before_request():
-        # if request.method != 'OPTIONS':
-        #     auth_header = request.headers.get('Authorization', None)
-        #
-        #     if not auth_header:
-        #         return jsonify({"error": "Missing authentication."}), 401
-        #
-        #     try:
-        #         token = auth_header.split(" ")[1]
-        #         decoded_token = auth.verify_id_token(token)
-        #         uid = decoded_token['uid']
-        #     except auth.InvalidIdTokenError:
-        #         return jsonify({"error": "Token is invalid."}), 401
-        pass
+        if current_app.config.get("AUTH_ENABLED") and request.method != 'OPTIONS':
+            auth_header = request.headers.get('Authorization', None)
+
+            if not auth_header:
+                return jsonify({"error": "Missing authentication."}), 401
+
+            try:
+                token = auth_header.split(" ")[1]
+                decoded_token = auth.verify_id_token(token)
+                uid = decoded_token['uid']
+            except auth.InvalidIdTokenError:
+                return jsonify({"error": "Token is invalid."}), 401
