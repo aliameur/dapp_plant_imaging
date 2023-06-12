62b6302a3f12abd1687f7260116c1f73e084c21d
aliameur <aliameur169@gmail.com>
2023-06-12 07:15:48 +0100

    recreate rpi plant board

--- rpi/plants/controller.py
+++ rpi/plants/controller.py
@@ -1,9 +1,32 @@
-import RPi.GPIO as GPIO
+import gpiozero
 from simple_pid import PID
 import ast
 import threading
 import time
+import board
 import neopixel
+from adafruit_tca9548a import TCA9548A
+from adafruit_bh1750 import BH1750
+from lm75 import LM75
+
+MUX_ADDRESS = 0x70
+LIGHT_SENSOR_ADDRESS = 0x23
+TEMP_SENSOR_ADDRESS = 0x48
+MUX = TCA9548A(board.I2C())
+FREQ = 100
+
+TEMP_Kp = 1
+TEMP_Ki = 0.1
+TEMP_Kd = 0.05
+BRIGHTNESS_Kp = 1
+BRIGHTNESS_Ki = 0.1
+BRIGHTNESS_Kd = 0.05
+
+
+# TODO convert from lux to brightness, need to test sensors
+# TODO convert from temp in C to heater output, need to test
+# TODO check if control loop works correctly
+# TODO check if we can physically use pwm with relays, or will we need solid state relays
 
 
 class Controller:
@@ -17,21 +40,38 @@ class Controller:
             "wavelength": 700,
             "brightness": 80
         }
+        self.led_strip = neopixel.NeoPixel(board.D6, 8, auto_write=False)
 
     def init_plants(self, plant_dict: str) -> str:
         try:
             plant_dict = self._string_to_dict(plant_dict)
             self.plants = plant_dict
+            for plant_id, plant in plants.items():
+                self.init_plant(plant, plant_id)
         except Exception as e:
             return e.__str__()
 
+    def init_plant(self, plant: dict, plant_id: str):
+        plant["temperature_pid"] = PID(TEMP_Kp, TEMP_Ki, TEMP_Kd,
+                                       setpoint=plant["ideal"]["temperature"])
+        plant["brightness_pid"] = PID(BRIGHTNESS_Kp, BRIGHTNESS_Ki, BRIGHTNESS_Kd,
+                                      setpoint=plant["ideal"]["brightness"])
+        plant["running"] = True
+
+        pin = plant["settings"]["heating_element_pin"]
+        plant["pwm"] = gpiozero.PWMOutputDevice(pin, frequency=FREQ, initial_value=0)
+
+        t = threading.Thread(target=self._control_loop, args=(1, plant_id))
+        t.start()
+        self.threads[plant_id] = t
+
     def get_current_conditions(self, plant_id: str) -> str:
         if plant_id == 'all':
-            # get all
-            pass
+            data = self.extract_current(self.plants)
+            return str(data)
         else:
-            # get one
-            pass
+            data = self.extract_current_single(self.plants[plant_id], plant_id)
+            return str(data)
 
     def set_wavelength(self, plant_id: str, wavelength: int):
         pass
@@ -72,26 +112,130 @@ class Controller:
         except Exception as e:
             return e.__str__()
 
-    def _plant_loop(self, plant_id: str):
-        while self.plants[plant_id]["running"]:
-            current_temp = self._read_temperature(plant_id)
-            control_value = self.plants[plant_id]["pid"](current_temp)
-            # TODO adjust heating element w/ control_value
-            time.sleep(1)  # may need to adjust
+    def read_temperature(self, plant_id: str) -> float:
+        sensor = LM75(MUX[self.plants[plant_id]["multiplexer_channel"]])
+        return sensor.getCelsius()
 
-    def _read_temperature(self, plant_id: str):
-        # placeholder for now, replace with actual function to read temperature from sensor
-        pass
+    def read_brightness(self, plant_id: str) -> float:
+        sensor = BH1750(MUX[self.plants[plant_id]["multiplexer_channel"]])
+        return sensor.lux
+
+    def control_heating(self, plant_id: str, duty_cycle: float):
+        pwm = self.plants[plant_id]["settings"]["pwm"]
+        # assume converted temperature to appropriate pwm value
+        pwm.value = duty_cycle
+
+    def control_led(self, plant_id: str, brightness: float):
+
+        start = self.plants[plant_id]["settings"]["led_start_number"]
+        rgb = self.wavelength_to_rgb(self.plants[plant_id]["ideal"]["wavelength"])
+
+        rgb_adjusted = self.adjust_brightness(rgb, brightness)
+
+        self.led_strip[start:start + 6] = rgb_adjusted * 6
+        self.led_strip.show()
+
+    def _control_loop(self, time_per_loop: int, plant_id: str):
+        while self.plants[plant_id]["running"]:
+            current_temperature = self.read_temperature(self.plants[plant_id])  # in C
+            current_brightness = self.read_brightness(self.plants[plant_id])  # in lux
+
+            self.plants["plant_id"]["current"]["temperature"] = current_temperature
+            self.plants["plant_id"]["current"]["brightness"] = current_brightness
+
+            temperature_control_value = self.plants[plant_id]["temperature_pid"](current_temperature)
+            brightness_control_value = self.plants[plant_id]["brightness_pid"](current_brightness)
+
+            self.control_heating(plant_id, temperature_control_value)
+            self.control_led(plant_id, brightness_control_value)
+
+            time.sleep(time_per_loop)
 
     def to_dict(self) -> dict:
         return self.plants
 
     @staticmethod
-    def _wavelength_to_rgb(wavelength: int) -> tuple:
-        # use self.calibration to convert to rgb, placeholder for now
-        rgb = (0, 0, 0)
+    def wavelength_to_rgb(wavelength):
+        gamma = 0.80
+        intensity_max = 255
+        factor, red, green, blue = 0.0, 0, 0, 0
+
+        wavelength = float(wavelength)
+
+        if 380 <= wavelength < 440:
+            red = -(wavelength - 440) / (440 - 380)
+            blue = 1.0
+        elif 440 <= wavelength < 490:
+            green = (wavelength - 440) / (490 - 440)
+            blue = 1.0
+        elif 490 <= wavelength < 510:
+            green = 1.0
+            blue = -(wavelength - 510) / (510 - 490)
+        elif 510 <= wavelength < 580:
+            red = (wavelength - 510) / (580 - 510)
+            green = 1.0
+        elif 580 <= wavelength < 645:
+            red = 1.0
+            green = -(wavelength - 645) / (645 - 580)
+        elif 645 <= wavelength <= 780:
+            red = 1.0
+
+        # Let the intensity fall off near the vision limits
+        if 380 <= wavelength < 420:
+            factor = 0.3 + 0.7 * (wavelength - 380) / (420 - 380)
+        elif 420 <= wavelength <= 700:
+            factor = 1.0
+        elif 700 < wavelength <= 780:
+            factor = 0.3 + 0.7 * (780 - wavelength) / (780 - 700)
+
+        # Don't want 0^x = 1 for x <> 0
+        rgb = (0 if red == 0.0 else int(round(intensity_max * ((red * factor) ** gamma))),
+               0 if green == 0.0 else int(round(intensity_max * ((green * factor) ** gamma))),
+               0 if blue == 0.0 else int(round(intensity_max * ((blue * factor) ** gamma))))
+
         return rgb
+
+    @staticmethod
+    def adjust_brightness(rgb, brightness):
+        return tuple(round(brightness * component) for component in rgb)
 
     @staticmethod
     def _string_to_dict(string: str) -> dict:
         return ast.literal_eval(string)
+
+    @staticmethod
+    def extract_current(data: dict):
+        result = {}
+        for key, value in data.items():
+            result[key] = value["current"]
+        return result
+
+    @staticmethod
+    def extract_current_single(data: dict, key: str):
+        return {key: data["current"]}
+
+
+# EXAMPLE PLANT
+plants = {
+    "id": {
+        "settings": {
+            "heating_element_pin": 1,
+            "multiplexer_channel": 1,
+            "led_start_number": 1,
+        },
+        "ideal": {
+            "temperature": 10,
+            "brightness": 10,
+            "wavelength": 10,
+        },
+        "current": {
+            "temperature": 10,
+            "brightness": 10,
+            "wavelength": 10,
+        },
+        "temperature_pid": PID(1, 0.1, 0.05, setpoint=15),
+        "brightness_pid": PID(1, 0.1, 0.05, setpoint=15),
+        "pwm": gpiozero.PWMOutputDevice,
+        "running": True
+    }
+}
